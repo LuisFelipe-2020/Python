4ff4e0cd76e405e9529222f9cc8f9cf0e651026f
LuisFelipe-2020 <71673377+LuisFelipe-2020@users.noreply.github.com>
2020-10-31 10:19:24 -0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,19 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for i in range(0, 13):
+    print(12 * i)
+
+
